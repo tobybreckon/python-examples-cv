f1e9ea3256eb5026ad9bf1d5f44577c3ab6a7b9e
tobybreckon <toby.breckon@cranfield.ac.uk>
2016-11-01 13:58:50 +0000

    initial version

---
+++ kalman_tracking_live.py
@@ -0,0 +1,237 @@
+#####################################################################
+
+# Example : kalman filtering base don cam shift object track processing
+# from a video file specified on the command line (e.g. python FILE.py video_file)
+# or from an attached web camera
+
+# N.B. use mouse to select region
+
+# Author : Toby Breckon, toby.breckon@durham.ac.uk
+
+# Copyright (c) 2016 Toby Breckon
+#                    Durham University, UK
+# License : LGPL - http://www.gnu.org/licenses/lgpl.html
+
+# based in part on code from: Learning OpenCV 3 Computer Vision with Python
+# Chapater 8 code samples, Minichino / Howse
+
+#####################################################################
+
+import cv2
+import sys
+import math
+import numpy as np
+
+#####################################################################
+
+keep_processing = True;
+camera_to_use = 0; # 0 if you have one camera, 1 or > 1 otherwise
+
+selection_in_progress = False; # support interactive region selection
+
+#####################################################################
+
+# select a region using the mouse
+
+boxes = [];
+current_mouse_position = np.ones(2, dtype=np.int32);
+
+def on_mouse(event, x, y, flags, params):
+
+    global boxes;
+    global selection_in_progress;
+
+    current_mouse_position[0] = x;
+    current_mouse_position[1] = y;
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        boxes = [];
+        # print 'Start Mouse Position: '+str(x)+', '+str(y)
+        sbox = [x, y];
+        selection_in_progress = True;
+        boxes.append(sbox);
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        # print 'End Mouse Position: '+str(x)+', '+str(y)
+        ebox = [x, y];
+        selection_in_progress = False;
+        boxes.append(ebox);
+#####################################################################
+
+# return centre of a set of points representing a rectangle
+
+def center(points):
+    x = (points[0][0] + points[1][0] + points[2][0] + points[3][0]) / 4.0
+    y = (points[0][1] + points[1][1] + points[2][1] + points[3][1]) / 4.0
+    return np.array([np.float32(x), np.float32(y)], np.float32)
+
+#####################################################################
+
+# define video capture object
+
+cap = cv2.VideoCapture();
+
+# define display window name
+
+windowName = "Kalman Object Tracking"; # window name
+windowName2 = "Hue histogram back projection"; # window name
+windowNameSelection = "initial selected region";
+
+# init kalman filter object
+
+kalman = cv2.KalmanFilter(4,2)
+kalman.measurementMatrix = np.array([[1,0,0,0],[0,1,0,0]],np.float32)
+kalman.transitionMatrix = np.array([[1,0,1,0],[0,1,0,1],[0,0,1,0],[0,0,0,1]],np.float32)
+kalman.processNoiseCov = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]],np.float32) * 0.03
+
+measurement = np.array((2,1), np.float32)
+prediction = np.zeros((2,1), np.float32)
+
+# if command line arguments are provided try to read video_name
+# otherwise default to capture from attached H/W camera
+
+if (((len(sys.argv) == 2) and (cap.open(str(sys.argv[1]))))
+    or (cap.open(camera_to_use))):
+
+    # create window by name (note flags for resizable or not)
+
+    cv2.namedWindow(windowName, cv2.WINDOW_NORMAL);
+    cv2.namedWindow(windowName2, cv2.WINDOW_NORMAL);
+    cv2.namedWindow(windowNameSelection, cv2.WINDOW_NORMAL);
+
+    # set a mouse callback
+
+    cv2.setMouseCallback(windowName, on_mouse, 0);
+    cropped = False;
+
+    # Setup the termination criteria for search, either 10 iteration or
+    # move by at least 1 pixel pos. difference
+    term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+
+    while (keep_processing):
+
+        # if video file successfully open then read frame from video
+
+        if (cap.isOpened):
+            ret, frame = cap.read();
+
+        # start a timer (to see how long processing and display takes)
+
+        start_t = cv2.getTickCount();
+
+        # select region using the mouse and display it
+
+        if (len(boxes) > 1) and (boxes[0][1] < boxes[1][1]) and (boxes[0][0] < boxes[1][0]):
+            crop = frame[boxes[0][1]:boxes[1][1],boxes[0][0]:boxes[1][0]].copy()
+
+            h, w, c = crop.shape;   # size of template
+            if (h > 0) and (w > 0):
+                cropped = True;
+
+                # convert region to HSV
+
+                hsv_crop =  cv2.cvtColor(crop, cv2.COLOR_BGR2HSV);
+
+                # select all Hue values (0-> 180) but eliminate values with very low
+                # saturation or value (due to lack of useful colour information)
+
+                # ideally these threshold would be adjustable parameters
+
+                mask = cv2.inRange(hsv_crop, np.array((0., 60.,32.)), np.array((180.,255.,255.)));
+
+                # construct a histogram of hue values and normalized it
+
+                crop_hist = cv2.calcHist([hsv_crop],[0],mask,[180],[0,180]);
+                # cv2.normalize(crop_hist,crop_hist,0,255,cv2.NORM_MINMAX);
+
+                # set intial position of object
+
+                track_window = (boxes[0][0],boxes[0][1],boxes[1][0] - boxes[0][0],boxes[1][1] - boxes[0][1]);
+
+                cv2.imshow(windowNameSelection,crop);
+
+            # reset list of boxes
+
+            boxes = [];
+
+        # interactive display of selection box
+
+        if (selection_in_progress):
+            top_left = (boxes[0][0], boxes[0][1]);
+            bottom_right = (current_mouse_position[0], current_mouse_position[1]);
+            cv2.rectangle(frame,top_left, bottom_right, (0,255,0), 2);
+
+        # if we have a selected region
+
+        if (cropped):
+
+            # convert incoming image to HSV
+
+            img_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV);
+
+            img_bproject = cv2.calcBackProject([img_hsv],[0],crop_hist,[0,180],1);
+            cv2.imshow(windowName2,img_bproject);
+
+            # apply camshift to predict new location (observation)
+            # basic HSV histogram comparision with adaptive window size
+            # see : http://docs.opencv.org/3.1.0/db/df8/tutorial_py_meanshift.html
+            ret, track_window = cv2.CamShift(img_bproject, track_window, term_crit);
+
+            # draw observation on image
+            x,y,w,h = track_window;
+            frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0),2);
+
+            # extract centre of this observation as points
+
+            pts = cv2.boxPoints(ret)
+            pts = np.int0(pts)
+            # (cx, cy), radius = cv2.minEnclosingCircle(pts)
+
+            # use to correct kalman filter
+
+            kalman.correct(center(pts));
+
+            # get new kalman filter prediction
+
+            prediction = kalman.predict();
+
+            # draw predicton on image
+
+            frame = cv2.rectangle(frame, (prediction[0]-(0.5*w),prediction[1]-(0.5*h)), (prediction[0]+(0.5*w),prediction[1]+(0.5*h)), (0,255,0),2);
+
+        # display image
+
+        cv2.imshow(windowName,frame);
+
+        # stop the timer and convert to ms. (to see how long processing and display takes)
+
+        stop_t = ((cv2.getTickCount() - start_t)/cv2.getTickFrequency()) * 1000;
+
+        # start the event loop - essential
+
+        # cv2.waitKey() is a keyboard binding function (argument is the time in milliseconds).
+        # It waits for specified milliseconds for any keyboard event.
+        # If you press any key in that time, the program continues.
+        # If 0 is passed, it waits indefinitely for a key stroke.
+        # (bitwise and with 0xFF to extract least significant byte of multi-byte response)
+        # here we use a wait time in ms. that takes account of processing time already used in the loop
+
+        # wait 40ms or less depending on processing time taken (i.e. 1000ms / 25 fps = 40 ms)
+
+        key = cv2.waitKey(max(2, 40 - int(math.ceil(stop_t)))) & 0xFF;
+
+        # It can also be set to detect specific key strokes by recording which key is pressed
+
+        # e.g. if user presses "x" then exit
+
+        if (key == ord('x')):
+            keep_processing = False;
+
+    # close all windows
+
+    cv2.destroyAllWindows()
+
+else:
+    print("No video file specified or camera connected.");
+
+#####################################################################

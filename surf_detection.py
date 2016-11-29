f7ccdafb9b7302898d65599e269acd19012d4ee8
Toby Breckon <toby.breckon@durham.ac.uk>
2016-11-29 11:32:14 +0000

    camera index

--- surf_detection.py
+++ surf_detection.py
@@ -28,7 +28,7 @@ import numpy as np
 #####################################################################
 
 keep_processing = True;
-camera_to_use = 0; # 0 if you have one camera, 1 or > 1 otherwise
+camera_to_use = 1; # 0 if you have one camera, 1 or > 1 otherwise
 
 selection_in_progress = False; # support interactive region selection
 

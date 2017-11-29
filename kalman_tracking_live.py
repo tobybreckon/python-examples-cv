2bf6040b7ff4e19a0872f5820bed320570f03495
Toby Breckon <toby.breckon@durham.ac.uk>
2017-11-29 21:00:10 +0000

    remove typo - line 1

--- kalman_tracking_live.py
+++ kalman_tracking_live.py
@@ -1,4 +1,4 @@
-f#####################################################################
+#####################################################################
 
 # Example : kalman filtering based cam shift object track processing
 # from a video file specified on the command line (e.g. python FILE.py video_file)

823100728587294d7e1caf1a7b11a480cb15d33a
Toby Breckon <toby@breckon.me.uk>
2019-10-26 13:52:16 +0100

    update header + tidy comments

--- yolo.py
+++ yolo.py
@@ -9,9 +9,14 @@
 # Copyright (c) 2019 Toby Breckon, Durham University, UK
 # License : LGPL - http://www.gnu.org/licenses/lgpl.html
 
-# significant portions based in part on the tutorial and example available at:
+# Implements the You Only Look Once (YOLO) object detection architecture decribed in full in:
+# Redmon, J., & Farhadi, A. (2018). Yolov3: An incremental improvement. arXiv preprint arXiv:1804.02767.
+# https://pjreddie.com/media/files/papers/YOLOv3.pdf
+
+# This code: significant portions based in part on the tutorial and example available at:
 # https://www.learnopencv.com/deep-learning-based-object-detection-using-yolov3-with-opencv-python-c/
 # https://github.com/spmallick/learnopencv/blob/master/ObjectDetection-YOLO/object_detection_yolo.py
+# under LICENSE: https://github.com/spmallick/learnopencv/blob/master/ObjectDetection-YOLO/LICENSE
 
 # To use first download the following files:
 
@@ -32,7 +37,6 @@ import numpy as np
 keep_processing = True
 
 # parse command line arguments for camera ID or video file, and YOLO files
-
 parser = argparse.ArgumentParser(description='Perform ' + sys.argv[0] + ' example operation on incoming camera/video image')
 parser.add_argument("-c", "--camera_to_use", type=int, help="specify camera to use", default=0)
 parser.add_argument("-r", "--rescale", type=float, help="rescale image by this factor", default=1.0)
@@ -46,12 +50,15 @@ args = parser.parse_args()
 
 ################################################################################
 # dummy on trackbar callback function
-
 def on_trackbar(val):
     return
 
 #####################################################################
 # Draw the predicted bounding box on the specified image
+# image: image detection performed on
+# class_name: string name of detected object_detection
+# left, top, right, bottom: rectangle parameters for detection
+# colour: to draw detection rectangle in
 
 def drawPred(image, class_name, confidence, left, top, right, bottom, colour):
     # Draw a bounding box.
@@ -63,16 +70,20 @@ def drawPred(image, class_name, confidence, left, top, right, bottom, colour):
     #Display the label at the top of the bounding box
     labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
     top = max(top, labelSize[1])
-    cv2.rectangle(frame, (left, top - round(1.5*labelSize[1])),
+    cv2.rectangle(image, (left, top - round(1.5*labelSize[1])),
         (left + round(1.5*labelSize[0]), top + baseLine), (255, 255, 255), cv2.FILLED)
-    cv2.putText(frame, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,0,0), 1)
+    cv2.putText(image, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,0,0), 1)
 
 #####################################################################
 # Remove the bounding boxes with low confidence using non-maxima suppression
+# image: image detection performed on
+# results: output from YOLO CNN network
+# threshold_confidence: threshold on keeping detection
+# threshold_nms: threshold used in non maximum suppression
 
-def postprocess(frame, results, threshold_confidence, threshold_nms):
-    frameHeight = frame.shape[0]
-    frameWidth = frame.shape[1]
+def postprocess(image, results, threshold_confidence, threshold_nms):
+    frameHeight = image.shape[0]
+    frameWidth = image.shape[1]
 
     classIds = []
     confidences = []
@@ -103,8 +114,7 @@ def postprocess(frame, results, threshold_confidence, threshold_nms):
                 boxes.append([left, top, width, height])
 
     # Perform non maximum suppression to eliminate redundant overlapping boxes with
-    # lower confidences.
-
+    # lower confidences
     classIds_nms = []
     confidences_nms = []
     boxes_nms = []
@@ -117,11 +127,11 @@ def postprocess(frame, results, threshold_confidence, threshold_nms):
         boxes_nms.append(boxes[i])
 
     # return post processed lists of classIds, confidences and bounding boxes
-
     return (classIds_nms, confidences_nms, boxes_nms)
 
 ################################################################################
-# Get the names of the output layers of the network
+# Get the names of the output layers of the CNN network
+# net : an OpenCV DNN module network object
 
 def getOutputsNames(net):
     # Get the names of all the layers in the network
@@ -186,37 +196,30 @@ if (((args.video_file) and (cap.open(str(args.video_file))))
     or (cap.open(args.camera_to_use))):
 
     # create window by name (as resizable)
-
     cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
 
     while (keep_processing):
 
         # start a timer (to see how long processing and display takes)
-
         start_t = cv2.getTickCount()
 
         # if camera /video file successfully open then read frame
-
         if (cap.isOpened):
             ret, frame = cap.read()
 
             # when we reach the end of the video (file) exit cleanly
-
             if (ret == 0):
                 keep_processing = False
                 continue
 
             # rescale if specified
-
             if (args.rescale != 1.0):
                 frame = cv2.resize(frame, (0, 0), fx=args.rescale, fy=args.rescale)
 
         # create a 4D tensor (OpenCV 'blob') from image frame (pixels scaled 0->1, image resized)
-
         tensor = cv2.dnn.blobFromImage(frame, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
 
         # set the input to the CNN network
-
         net.setInput(tensor)
 
         # runs forward inference to get output of the final output layers
@@ -227,7 +230,6 @@ if (((args.video_file) and (cap.open(str(args.video_file))))
         classIDs, confidences, boxes = postprocess(frame, results, confThreshold, nmsThreshold)
 
         # draw resulting detections on image
-
         for detected_object in range(0, len(boxes)):
             box = boxes[detected_object]
             left = box[0]
@@ -242,30 +244,24 @@ if (((args.video_file) and (cap.open(str(args.video_file))))
         cv2.putText(frame, label, (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
 
         # display image
-
         cv2.imshow(windowName,frame)
         cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN,
                                 cv2.WINDOW_FULLSCREEN & args.fullscreen)
 
         # stop the timer and convert to ms. (to see how long processing and display takes)
-
         stop_t = ((cv2.getTickCount() - start_t)/cv2.getTickFrequency()) * 1000
 
         # start the event loop + detect specific key strokes
-
         # wait 40ms or less depending on processing time taken (i.e. 1000ms / 25 fps = 40 ms)
-
         key = cv2.waitKey(max(2, 40 - int(math.ceil(stop_t)))) & 0xFF
 
         # if user presses "x" then exit  / press "f" for fullscreen display
-
         if (key == ord('x')):
             keep_processing = False
         elif (key == ord('f')):
             args.fullscreen = not(args.fullscreen)
 
     # close all windows
-
     cv2.destroyAllWindows()
 
 else:

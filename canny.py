a24581cb0459743710171b0f404dd5514895a0b7
tobybreckon <toby.breckon@durham.ac.uk>
2024-02-29 01:33:34 +0000

    add -s option to override default camera resolution

--- canny.py
+++ canny.py
@@ -39,6 +39,12 @@ parser.add_argument(
     help="rescale image by this factor",
     default=1.0)
 parser.add_argument(
+    "-s",
+    "--set_resolution",
+    type=int,
+    nargs=2,
+    help='override default camera resolution as H W')
+parser.add_argument(
     'video_file',
     metavar='video_file',
     type=str,
@@ -106,6 +112,12 @@ if (((args.video_file) and (cap.open(str(args.video_file))))
         nothing)
     sobel_size = 3  # greater than 7 seems to crash
     cv2.createTrackbar("sobel size", window_name2, sobel_size, 7, nothing)
+
+    # override default camera resolution
+
+    if not(args.set_resolution is None):
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, args.set_resolution[1])
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, args.set_resolution[0])
 
     while (keep_processing):
 
